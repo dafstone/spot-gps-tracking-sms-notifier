b03613c130cda883f03510a17a60b801f13934b1
Dan Stone <dan@evolvingmedia.net>
2016-12-12 09:11:02 -0500

    Initialize @TwilioService

--- main.rb
+++ main.rb
@@ -2,13 +2,13 @@ require './dependencies'
 
 # Require Configuration File
 CONFIG = YAML.load_file('./config/api_configurations.yml')
-
 SHIP_NAME = CONFIG[:ship_name]
 
 # Require Application Libraries (MVC Loader)
 APPLICATION_LIB_DIR = Dir['./app/helpers/*.rb'].sort + Dir['./app/models/**/*.rb'].sort + Dir['./app/controllers/**/*.rb']
 APPLICATION_LIB_DIR.each { |file| require file }
 
+@twilio_service = TwilioService.new(CONFIG[:twilio])
 @songbird_locations = SpotService.new(spot_feed_id: CONFIG[:spot_feed_id])
 @smp = SpotMessageProcessor.new
 
