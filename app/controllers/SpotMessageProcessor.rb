2ab7d245f837eb29475f6a13ecc3f31d977ddf59
Dan Stone <dan@evolvingmedia.net>
2016-12-11 22:33:06 -0500

    SpotMessageProcessor will only import messsages not already in the database

--- app/controllers/SpotMessageProcessor.rb
+++ app/controllers/SpotMessageProcessor.rb
@@ -1,14 +1,16 @@
 class SpotMessageProcessor
   def import_spot_messages(messages)
     messages.each do |m|
-      SpotMessage.create(
-        message_type: m[:messageType],
-        latitude: m[:latitude],
-        longitude: m[:longitude],
-        message_time: m[:dateTime],
-        message_id: m[:id],
-        message_content: m[:messageContent]
-      )
+      if SpotMessage.where(message_id: m[:id]).exists? != true
+        SpotMessage.create(
+          message_type: m[:messageType],
+          latitude: m[:latitude],
+          longitude: m[:longitude],
+          message_time: m[:dateTime],
+          message_id: m[:id],
+          message_content: m[:messageContent]
+        )
+      end
     end
   end
 end
